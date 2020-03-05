0348599f4d65bbf873432017271833e0b69a6be0
BiggMikeDeLegend <59550409+BiggMikeDeLegend@users.noreply.github.com>
2020-03-04 23:07:21 -0500

    Initial commit

---
+++ venv/Unnamed_0x00.py
@@ -0,0 +1,10 @@
+class Serf:
+    def __init__(self):
+        # General Attributes:
+        self.settlement = None
+        self.home = None
+        # Person Attributes:
+        self.weight = 100
+        self.job = None
+        self.skills = None
+        self.
---
+++ venv/testing_a1.py
@@ -0,0 +1,24 @@
+import pygame
+
+pygame.init()
+
+clock = pygame.time.Clock()
+Running = True
+
+pygame.display.set_caption("Unnamed Program")
+
+display_width = 700
+display_height = 700
+
+display = pygame.display.set_mode((display_width,display_height))
+
+while Running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:
+            Running = False
+
+    clock.tick(120)
+
+pygame.quit()
+quit()
