aba690772e7ff3b2c38daaf53d665640c4dfc35e
BiggMikeDeLegend <59550409+BiggMikeDeLegend@users.noreply.github.com>
2020-03-04 23:38:08 -0500

    Test 2

--- venv/Unnamed_0x00.py
+++ venv/Unnamed_0x00.py
@@ -1,10 +1,34 @@
-class Serf:
+class Hombre:
     def __init__(self):
         # General Attributes:
         self.settlement = None
         self.home = None
         # Person Attributes:
+        self.names = self.fetch_random_name() # Also need to acount for lineage and passing down of last names
         self.weight = 100
+        self.socialClass = 0
+        self.wealth = 0
+        self.family = None
+        # Profesion Attributes:
         self.job = None
         self.skills = None
-        self.
+        self.employer = None
+
+    @staticmethod
+    def fetch_random_name():
+        names = ["Freddie", "Earl", "Von Hickter"]
+        return names
+
+class Settlement:
+    def __init__(self):
+        self.tier = 0
+        self.size = 1 # IDK??
+        self.population = None
+        self.people = None
+        self.num_building = 0
+        self.buildings = []
+
+class building:
+    def __init__(self):
+        self.type = None
+        self.name - self.type.name
